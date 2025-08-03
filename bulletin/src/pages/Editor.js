51a3c6d1dfa5d376b1f1b562d0f1539eeaeb695d
luke <luke.taful@gmail.com>
2025-08-03 13:24:19 +0100

    moving across basic structure and blueprint generation from old files. editor not working, base element loading is

---
+++ bulletin/src/pages/Editor.js
@@ -0,0 +1,133 @@
+import {useState} from 'react';
+import blueprint from '../../public/blueprint.json';
+import Draggable, {DraggableCore} from 'react-draggable';
+import logo from '../../public/vercel.svg';
+import Popup from 'reactjs-popup';
+
+
+
+
+export default function EditorBoard(){
+
+//Handlers for adding elements
+  const[value, setValue] = useState("");
+  //I have no idea why, but unless I handle the onclick like this, it adds a new element onChange
+  function handle() {
+    AddText(value);
+  }
+
+  return(
+    <div id="board">
+      {/* Creating the custom elements */}
+      {CreateElements(blueprint)}
+      <div>
+        {/* Adding buttons for customization options */}
+          <button onClick={SaveState}>Save</button>
+          {/* popup to add new text */}
+          <Popup trigger={<button> Add Text</button>} position="left center"> 
+            <h4>Add Text:</h4>
+            <input type="text" id="textIn"  onChange={(e) => {setValue(e.target.value)}}/><br/>
+            <button id="atButton" onClick={handle}>Enter</button>
+          </Popup>
+          {/* popup to add new image */}
+          <button onClick={AddImage}>Add Image</button>
+      </div>
+      
+    </div>
+  );
+}
+
+function CreateElements(items){
+  //Create each custom element from the json blueprint and store in a list.
+  const elements = items.map(items => {
+    const tempElements = [];
+      if(items.type == "text"){
+        tempElements.push(
+          MakeText(items)
+      )}
+      if(items.type == "img"){
+        tempElements.push(
+          MakeImg(items)
+      )}
+    console.log("returning elements")
+    return tempElements;
+  })
+  //Wrap in another div, as react can only return 1 element per function.
+  return(
+    <div id="loadContainer">
+      {elements}
+    </div>
+  )
+}
+
+function MakeText(items){   //Handles creating a new element with text 
+  //These must be draggable in the board editor. This tracks their location so it can be saved
+  const handleStop = (event, dragElement) => {
+    var xpos = dragElement.x;
+    var ypos = dragElement.y;
+    console.log(xpos, ypos);
+    items.xpos = xpos;
+    items.ypos = ypos;
+    console.log(items.xpos, items.ypos);
+  };
+  return(
+  <Draggable
+          grid={[25, 25]}
+          scale={1}
+          onStop={handleStop}>
+          <div key={items.id}>
+            <p id={items.id} style={{
+              color: items.color, fontFamily: items.font, fontSize: items.textSize,
+              position: 'absolute', top: items.ypos, left: items.xpos
+              }}>
+                {items.text}</p>
+          </div>
+          </Draggable>
+  )}
+
+function MakeImg(items){    //Handles creating a new element with text 
+//Location Tracking when the div is dragged
+  const tracker = (event, dragElement) => {
+    var xpos = dragElement.x;
+    var ypos = dragElement.y;
+    console.log(xpos, ypos);
+    items.xpos = xpos;
+    items.ypos = ypos;
+    console.log(items.xpos, items.ypos);
+  };
+  return(
+  <Draggable
+    grid={[25, 25]}
+    scale={1}
+    onStop={tracker}>
+    <div key={items.id}>
+      <img id={items.id} src={logo} alt={items.text} width={items.size} height={items.size}
+      style={{position:'absolute', top: items.ypos, left: items.xpos}}
+      ></img>
+    </div>
+  </Draggable>
+)}
+
+function AddText(textIn){
+    blueprint.push({
+        id:4,
+       type:"text",
+        text: textIn, 
+        color:"white",
+        font:"Arial, Helvetica, sans-serif",
+        textSize:"smaller",
+        xpos:100,
+        ypos:200
+    });
+    console.log(blueprint);
+}
+
+
+function AddImage(){
+     //Open local file explorer
+     //There should be some file type checking here
+}
+
+function SaveState(blueprint){
+  const blueprintString = JSON.stringify(blueprint);
+}
---
+++ bulletin/src/pages/Home.js
@@ -0,0 +1,44 @@
+import blueprint from '../../public/blueprint.json';
+import logo from '../../public/vercel.svg';
+
+
+export default function Board(){
+    return(
+      <div id="board">
+        {CreateElements(blueprint)}
+      </div>
+    )
+  }
+
+function CreateElements(items){
+  const elements = items.map(items => {
+    const tempElements = [];
+
+      if(items.type == "text"){
+      
+        tempElements.push(
+          <div key={items.id}>
+            <p style={{
+              color: items.color, fontFamily: items.font, fontSize: items.textSize,
+              position: 'absolute', top: items.ypos, left: items.xpos
+              }}>
+                {items.text}</p>
+          </div>
+      )}
+      if(items.type == "img"){
+        tempElements.push(
+          <div key={items.id}>
+            <img src={logo} alt={items.text} width={items.size} height={items.size}
+            style={{position:'absolute', top: items.ypos, left: items.xpos}}
+            ></img>
+          </div>
+      )}
+    
+    return tempElements;
+  })
+  return(
+    <div id="loadContainer">
+      {elements}
+    </div>
+  )
+}
