d0d8ced93bb9292a9cfbeaddb5e82d3b0d15714b
luke <luke.taful@gmail.com>
2025-08-08 17:15:51 +0100

    exposing the blueprint JSON through the server api. Recieving and outputing on the react side. Also fixed board/home naming error

--- bulletin/src/app/page.js
+++ bulletin/src/app/page.js
@@ -2,11 +2,30 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import layout from "./layout.js";
 
+async function getBP(){
+  const url = "http://localhost:5000";
+  const data = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json', 
+    }};
+  const response = await fetch(url);
+  const out = (await response.json());
+  return out;
+}
+
+
 export default function Home() {
+
+  var blueprint = JSON.stringify({state:"empty"});
+  const recieveBP = async() => {blueprint = await getBP(); console.log(blueprint);}
+  recieveBP()
+  console.log(blueprint);
+
   return (
     <div className={styles.page}>
       <layout>
-        <p>Hello World!</p>
+        <p>hello world!</p>
       </layout>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const Express = require("express");
+const blueprint = require("./blueprint.json");
 // import Cors from "cors";
 const app = Express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(blueprint);
 })
 
 app.listen(port, () => {
 
-    console.log('Listening on port ${port}')
+    console.log('Listening on port ' + port);
 
 })
