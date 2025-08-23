886071ab5238cf42948ad406e8d5cc25fc30a67c
luke <luke.taful@gmail.com>
2025-08-23 16:50:29 +0100

    cleaning up board

--- bulletin/src/pages/Board.js
+++ bulletin/src/pages/Board.js
@@ -1,9 +1,7 @@
 "use client"
-import localBP from "../../public/blueprint.json"
 import React, {useEffect, useState} from "react";
 import { Editor } from "./Editor"
 import Draggable from 'react-draggable';
-
 
 export default function Board(){
 
@@ -34,46 +32,11 @@ export default function Board(){
 
   return(
     <div id="board">
-      {CreateElements(blueprint)}
       <button onClick={handleClick}>Edit</button>
+      {CreateElements(blueprint)}
     </div>
   )
 }
-
-
-// function CreateElements(items){
-//   const elements = items.map(items => {
-//     const tempElements = [];
-
-//       if(items.type == "text"){
-//         tempElements.push(
-//           <div key={items.id}>
-//             <p style={{
-//               color: items.color, fontFamily: items.font, fontSize: items.textSize,
-//               position: 'absolute', top: items.ypos, left: items.xpos
-//               }}>
-//                 {items.text}</p>
-//           </div>
-//       )}
-//       if(items.type == "img"){
-//         tempElements.push(
-//           <div key={items.id}>
-//             <img src={items.src} alt={items.text} width={items.size} height={items.size}
-//             style={{position:'absolute', top: items.ypos, left: items.xpos}}
-//             ></img>
-//           </div>
-//       )}
-
-//     return tempElements;
-//   })
-//   return(
-//     <div id="loadContainer">
-//       {elements}
-//     </div>
-//   )
-// }
-
-
 
 
 function CreateElements(items){
